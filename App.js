fa94fb930aad274fa9487dd717490dab5372d715
Manjunatha Veeranna <mveeranna@synamedia.com>
2024-04-01 01:02:27 +0530

    parcel-package

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 // const heading = React.createElement("h1",{id : "heading"},"HELLO WORLD ! ");
+import React from "react";
+import  ReactDOM  from "react-dom";
 
 const heading = React.createElement(
   "div",
